3bebb8bf9f9d87469002cbb83b16a2b7737af900
Daara Shaw <dev@daara.me>
2025-02-15 13:44:13 +0000

    remove empty ruff lsp setup

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -78,15 +78,6 @@ return {
                             }
                         })
                     end,
-                    ruff = function()
-                        require('lspconfig').ruff.setup({
-                            init_options = {
-                                settings = {
-                                    -- Ruff language server settings go here
-                                }
-                            }
-                        })
-                    end,
                 },
             })
         end
