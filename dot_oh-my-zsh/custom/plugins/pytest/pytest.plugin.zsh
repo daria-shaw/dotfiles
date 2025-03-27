013d16c3d2a1772e963ee1a5ece003282103758b
Daara Shaw <d.shaw@draftkings.com>
2025-03-27 09:35:22 +0000

    pytest fix coverage shortcut

--- dot_oh-my-zsh/custom/plugins/pytest/pytest.plugin.zsh
+++ dot_oh-my-zsh/custom/plugins/pytest/pytest.plugin.zsh
@@ -1,4 +1,4 @@
 alias prp='poetry run pytest -v'
 alias prps='BUILD_NUMBER=1 prp'
 alias prpb='prp --benchmark-only --benchmark-enable'
-alias prpc='prp --cov=src --cov-report html'
+alias prpc='prp --cov=src --cov-report html --cov-append'
